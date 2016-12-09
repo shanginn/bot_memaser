702ceb6704431a9b7f8e6c7249d0000ab51c6590
Nikolay Shangin <shanginn@gmail.com>
2016-12-09 05:51:29 +0500

    Init

---
+++ bot.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+from telegram.ext import Updater, MessageHandler
+import logging, yaml, sys, os, urllib, urlparse
+from vk_manager import VKM
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.DEBUG,
+    filename='bot.log',
+    filemode='w',
+)
+
+logger = logging.getLogger(__name__)
+
+
+# Читаем конфиги
+try:
+    with open("config.yml", 'r') as ymlfile:
+        config = yaml.load(ymlfile)
+except BaseException:
+    print "config.yml file is not exists! Please create it first."
+    sys.exit()
+
+if config['telegram-token'] == '':
+    print "Please configure your Telegram bot token"
+    sys.exit()
+
+if config['vk-login'] == '' or config['vk-password'] == '':
+    print "Please add your Vkontakte credentials"
+    sys.exit()
+
+if config['vk-group-id'] == '':
+    print "Please add Vkontakte group id"
+    sys.exit()
+
+
+manager = VKM(config['vk-login'], config['vk-password'], config['vk-group-id'])
+
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+    bot.sendMessage(
+        update.message.chat_id,
+        text='Update "%s" caused error "%s"' % (update, error)
+    )
+
+
+def upload_image(bot, update):
+    if update.message.photo:
+        # Получаем фотку наилучшего качества(последнюю в массиве)
+        photo = update.message.photo[-1]
+
+        # Описание к фотке
+        caption = update.message['caption']
+
+
+        # url фото на сервере Telegram
+        image_url = bot.getFile(photo['file_id'])['file_path']
+
+        # Имя файла с корректным расширением из url
+        filename = urlparse.urlsplit(image_url.strip()).path.split('/')[-1]
+
+        # Загружаем фотку на диск
+        urllib.urlretrieve(image_url, filename)
+
+        # Загружаем фотку на стену группы Вконтакте
+        manager.upload_image(filename, caption)
+
+        # Удаляем фотку
+        os.remove(filename)
+
+
+def main():
+    updater = Updater(config['telegram-token'])
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    #dp.add_handler(CommandHandler("help", start))
+    dp.add_handler(MessageHandler(False, upload_image))
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Block until the you presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ vk_manager.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+import vk_api
+
+"""
+Менеджер Вконтакте.
+Используется vk_api (https://github.com/python273/vk_api)
+Загружает фотки на стену группы ВК.
+
+Я не нашел способ загрузки фоточек через токен,
+поэтому используется логин и пароль админа группы.
+"""
+
+class VKM:
+    def auth_handler(self):
+        # Если у вас включена 2-х факторная авторизация,
+        # то при первом запуске нужно ввести код
+        two_FA_code = int(raw_input("Please enter 2FA code: "))
+
+        return two_FA_code, True
+
+    def __init__(self, login, password, group_id):
+        vk_session = vk_api.VkApi(login, password, auth_handler=self.auth_handler)
+
+        try:
+            vk_session.authorization()
+        except vk_api.AuthorizationError as error_msg:
+            print(error_msg)
+            return
+
+        self.vk_session = vk_session
+        self.group_id = group_id
+        self.api = self.vk_session.get_api()
+
+    def upload_image(self, image_path, caption=''):
+        # Сначала нужно загрузть фотку на сервера ВК
+        upload = vk_api.VkUpload(self.vk_session)
+
+        photo = upload.photo_wall(
+            image_path,
+            group_id=self.group_id
+        )
+
+        # Потом получить её ID
+        vk_photo_id = 'photo{}_{}'.format(
+            photo[0]['owner_id'], photo[0]['id']
+        )
+
+        # И запостить на стену группы
+        self.api.wall.post(
+            owner_id='-%d' % self.group_id,
+            attachments=vk_photo_id,
+            message=caption
+        )
