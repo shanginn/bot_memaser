b7c7de41d7f9d476913161b01c7d1efc8e172f31
Shangin Nikolai <shanginn@gmail.com>
2018-04-17 23:42:14 +0500

    python3 in launcher

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cd /home/pi/Projects/bot_memaser/ && screen -dmS memaser /usr/bin/python bot.py
+cd /home/pi/Projects/bot_memaser/ && screen -dmS memaser /usr/bin/python3 bot.py
 
 exit 0
